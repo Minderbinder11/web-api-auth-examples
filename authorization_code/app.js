0a984b47266bbf9a3d7e136ba54039742ea9eb3b
Phil Barow <pbarow@gmail.com>
2017-05-16 08:47:08 -0700

    saving for transfer to MAC

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -12,9 +12,9 @@ var request = require('request'); // "Request" library
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = '9e81a2c4ff564c00b489ec6596d799dd'; // Your client id
+var client_secret = 'e86d0218a2974b87b11ed6ba24a2c887'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
